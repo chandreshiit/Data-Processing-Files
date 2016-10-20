6ef5d2558af0a5335ed05eb7f94d3a0593472133
Chandresh Maurya <ckm.jnu@gmail.com>
2016-10-20 11:59:52 +0530

    Update arff2libsvm.py

--- arff2libsvm.py
+++ arff2libsvm.py
@@ -1,3 +1,5 @@
+#Author: Chandresh Maurya
+# Contact: ckm.jnu@gmail.com
 import sys
 
 if(len(sys.argv) < 3):
@@ -13,19 +15,19 @@ fout = open(outputfilename,'w')
 
 beginToRead = False
 for line in lines:
-    if beginToRead == True:
-        if len(line) > 5:# not an empty line
-            #read this line
-            dataList = line.split(',')
-            resultLine = ''
-            resultLine += dataList[-1].strip()
-            resultLine += ' '
-            for i in range(1,len(dataList)-1):
-                resultLine += str(i)
-                resultLine += (":"+dataList[i]+" ")
-            #print(resultLine)
-            fout.write(resultLine+"\n")
-
-    if line[0:5] == '@data':
-        beginToRead = True
-
+   #  print(line)
+     if beginToRead:
+        # print("reading")
+         if line.strip(): # not an empty line
+              #read this line
+              dataList = line.split(',')
+              resultLine = ''
+              resultLine += dataList[-1].strip()
+              resultLine += ' '
+              for i in range(0,len(dataList)-1):
+                  resultLine += str(i)
+                  resultLine += (":"+dataList[i].strip()+" ")
+              print(resultLine)
+              fout.write(resultLine+"\n")
+     if line.find('@DATA') >=0 :
+         beginToRead = True
